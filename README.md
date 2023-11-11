3d3cdede32b70e753e35e8a532f8a98986960444
Max <max.prokopchuk@gmail.com>
2023-11-11 12:40:18 +0200

    IP Location Finder with Interactive Map This Python script serves as an IP location finder, leveraging external APIs to gather information about the geographical location associated with a given IP address. The script not only displays details about the IP address, such as country, region, city, and more but also generates an interactive map using the Folium library.
    Key Features
    IP Information Retrieval: Utilizes the requests library to query the http://ip-api.com API and retrieves detailed information about the specified IP address, including geographical coordinates, timezone, ISP, and more.
    
    Stylish Presentation: Incorporates the pyfiglet library to present the script's output in a stylized ASCII art format, enhancing the user experience.
    
    Interactive Map Creation: Integrates the folium library to dynamically generate an interactive map centered around the latitude and longitude coordinates obtained from the IP information.
    
    Usage Instructions
    Execute the script by running python main.py in the terminal.
    Enter the IP address when prompted.
    The script will display comprehensive details about the IP, and an HTML file containing an interactive map will be generated.
    Dependencies
    The script relies on the following Python libraries:
    
    requests: Facilitates HTTP requests to the IP geolocation API.
    pyfiglet: Enhances the visual appeal of the script's output through ASCII art.
    folium: Enables the creation of interactive maps for visualizing geographical data.

---
+++ README.md
@@ -0,0 +1,21 @@
+# IP Location Finder
+
+This script retrieves information about the location based on an IP address and creates an interactive map using the requests, pyfiglet, and folium libraries.
+
+## Usage
+
+1. Run the script: `python main.py`
+2. Enter the IP address when prompted.
+
+The script will display information about the location and generate an HTML file with an interactive map.
+
+## Environment Requirements
+
+- Python 3.x
+- Install dependencies with `pip install -r requirements.txt`
+
+
+
+## Contributions
+
+Feel free to contribute by opening issues or submitting pull requests.
---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+from pyfiglet import Figlet
+import folium
+
+def get_info_by_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        #print(response)
+        data = {
+            '[IP]': response.get('query', 'N/A'),
+            '[Country]': response.get('country', 'N/A'),
+            '[Region Name]': response.get('regionName', 'N/A'),
+            '[Region Code]': response.get('regionCode', 'N/A'),
+            '[City]': response.get('city', 'N/A'),
+            '[Zip Code]': response.get('zip', 'N/A'),
+            '[Lat]': response.get('lat', 'N/A'),
+            '[Lon]': response.get('lon', 'N/A'),
+            '[Timezone]': response.get('timezone', 'N/A'),
+            '[ISP]': response.get('isp', 'N/A'),
+            '[Org]': response.get('org', 'N/A'),
+            '[AS]': response.get('as', 'N/A'),
+            '[AS Name]': response.get('asname', 'N/A'),
+            '[Hostname]': response.get('hostname', 'N/A'),
+        }
+        for key, value in data.items():
+            print(f'{key}: {value}')
+
+            area = folium.Map(location=[response.get('lat'), response.get('lon')], zoom_start=10)
+            area.save(f'{response.get("query", "N/A")}_{response.get("city", "N/A")}.html')
+
+    except requests.exceptions.ConnectionError:
+        print('Connection Error')
+
+def main():
+    previous = Figlet(font='slant')
+    print(previous.renderText('IP Address'))
+    ip = input('Enter IP: ')
+    get_info_by_ip(ip=ip)
+
+
+if __name__ == '__main__':
+    main()
